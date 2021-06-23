cd84bd8f23e837bb917f825d3961e4151349be08
Martin Gropp <martin.gropp@gmail.com>
2021-06-23 22:32:43 +0200

    adapted alex's install script

--- setup/ubuntu/install.sh
+++ setup/ubuntu/install.sh
@@ -1,39 +1,78 @@
 #!/bin/bash
-# By Alex Zaslavskis.
+# By Alex Zaslavskis and Martin Gropp
+set -e
+set -u
 
-echo Java should be installed on your system...
-if command -v java >/dev/null 2>&1 ; then
-    echo "java found"
-    echo "version: $(java -version)"
+dest_dir="/opt/pdfjumbler"
+desktop_file="/usr/share/applications/pdfjumbler.desktop"
+desktop_file_url="https://raw.githubusercontent.com/alex5250/PdfJumbler-Installer-Linux/main/config.config"
+jar_file="$dest_dir/pdfjumbler.jar"
+jar_file_url=$(
+	curl  https://api.github.com/repos/mgropp/pdfjumbler/releases/latest \
+	| grep browser_download_url \
+	| grep .jar \
+	| cut -d '"' -f 4 \
+)
+launcher_file="/usr/local/bin/pdfjumbler"
+
+# Make sure Java is installed
+if command -v java >/dev/null 2>&1
+then
+    echo "Java found."
+    echo "Version:"
+	java -version
 else
-    echo "java not found. Please install java"
+    echo "Java not found. Please install Java!"
     exit 1
 fi
 
+# Create temp directory
+tmpdir=$( mktemp -d )
+#trap "rm -rf \"$tmpdir\"" EXIT
+echo "Temporary directory: $tmpdir"
+cd "$tmpdir"
 
+# Create launcher
+cat > pdfjumbler << EOL
+#!/bin/sh
+exec java -jar "$jar_file" "\$@"
+EOL
 
+# Create desktop file
+cat > pdfjumbler.desktop << EOL
+[Desktop Entry]
+Name=PdfJumbler
+Exec=$launcher_file
+Icon=pdf
+Type=Application
+Categories=GTK;GNOME;Utility;
+Terminal=false
+EOL
 
-echo Clone all files 
- curl   https://raw.githubusercontent.com/alex5250/PdfJumbler-Installer-Linux/main/config.config  -L  -o /tmp/pdfjumbler.desktop
+# Create script to move files and set permissions
+cat > install.sh << EOF
+#!/bin/sh
+mv pdfjumbler.desktop "$desktop_file"
+chown root:root "$desktop_file"
+chmod 644 "$desktop_file"
 
-sudo mv /tmp/pdfjumbler.desktop /usr/share/applications/pdfjumbler.desktop
-sudo chmod  644  /usr/share/applications/pdfjumbler.desktop
-mkdir -p /opt/pdfjumbler
+mkdir -p "$dest_dir"
+mv pdfjumbler.jar "$jar_file"
+chown -R root:root "$dest_dir"
+chmod 755 "$dest_dir"
+chmod 644 "$jar_file"
 
-DOWNLOAD_URL=$(curl  https://api.github.com/repos/mgropp/pdfjumbler/releases/latest \
-        | grep browser_download_url \
-        | grep .jar \
-        | cut -d '"' -f 4)
-sudo curl  -L  -o /tmp/pdfjumbler.jar   "$DOWNLOAD_URL"
- sudo mv /tmp/pdfjumbler.jar /opt/pdfjumbler/pdfjumbler.jar
-sudo chmod  755  /opt/pdfjumbler/pdfjumbler.jar
-echo "exec java -jar /opt/pdfjumbler/pdfjumbler.jar "$@" " > /tmp/pdfjumbler.sh
+mv pdfjumbler "$launcher_file"
+chown root:root "$launcher_file"
+chmod 755 "$launcher_file"
+EOF
 
-sudo mv /tmp/pdfjumbler.sh /usr/local/bin/pdfjumbler
+# Download
+echo "Downloading files"
+curl -L "$jar_file_url" -o pdfjumbler.jar
 
+# Install
+#sudo -c "sh install.sh"
 
-sudo chmod 755 /usr/local/bin/pdfjumbler
-
-
-
-echo Done ...Enjoy 
+echo
+echo "Done. Enjoy!"
